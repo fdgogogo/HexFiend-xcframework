ba7f52864c437ffcabad53fb03e1afbff70d05ed
Jiaan Fang <jiaanf@gmail.com>
2023-12-13 15:25:37 +0800

    initial commit (2.17.1)

--- Package.swift
+++ Package.swift
@@ -14,10 +14,9 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "HexFiend-xcframework"),
-        .testTarget(
-            name: "HexFiend-xcframeworkTests",
-            dependencies: ["HexFiend-xcframework"]),
+        .binaryTarget(
+            name: "HexFiend-xcframework",
+            path: "HexFiend.xcframework"
+        ),
     ]
 )
---
+++ README.md
@@ -0,0 +1,6 @@
+#  HexFiend Framework 
+
+Prebuilt [HexFiend](https://github.com/HexFiend/HexFiend) binary framework for iOS and macOS. 
+For use in SwiftPM
+
+
